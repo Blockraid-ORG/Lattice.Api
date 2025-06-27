d99e40cd742c6f8dc26fe62de15ba3a2d554d212
Uut Budiarto <87194683+uutbudiarto2012@users.noreply.github.com>
2025-06-27 10:02:34 +0700

    Project (#18)
    * coba ubah docker
    
    * coba ubah docker 1
    
    * ok
    
    * sleep
    
    * -
    
    * - -
    
    * create find get product
    
    ---------
    
    Co-authored-by: jawaskrip2025 <jawaskrip@gmail.com>

--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateProjectDto } from './dto/create-project-dto';
-
+import { CreateProjectDto, UpdateProjectDto } from './dto/create-project-dto';
+import { QueryParamDto } from 'src/common/pagination/dto/pagination.dto';
+import { createPaginator } from 'prisma-pagination';
+import { Prisma, Project } from '@prisma/client';
+import { parseBoolean } from 'src/common/utils/parse-data-type';
 @Injectable()
 export class ProjectsService {
   constructor(private readonly prisma: PrismaService) {}
@@ -44,6 +47,220 @@ export class ProjectsService {
         allocations: true,
         socials: { include: { social: true } },
         Presales: true,
+      },
+    });
+  }
+  async findMany(query: QueryParamDto) {
+    if (parseBoolean(query.noPaginate)) {
+      return this.noPagination(query);
+    }
+    return this.withPagination(query);
+  }
+  async findOne(id: string) {
+    const result = await this.prisma.project.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        slug: true,
+        logo: true,
+        banner: true,
+        ticker: true,
+        decimals: true,
+        totalSupply: true,
+        detail: true,
+        status: true,
+        reviewLogs: {
+          select: {
+            id: true,
+            status: true,
+            note: true,
+          },
+        },
+        socials: {
+          select: {
+            url: true,
+            social: {
+              select: {
+                id: true,
+                name: true,
+                icon: true,
+              },
+            },
+          },
+        },
+        category: {
+          select: {
+            id: true,
+            name: true,
+            icon: true,
+          },
+        },
+        chains: {
+          select: {
+            chain: {
+              select: {
+                id: true,
+                name: true,
+                logo: true,
+              },
+            },
+          },
+        },
+        allocations: {
+          select: {
+            id: true,
+            name: true,
+            supply: true,
+            vesting: true,
+            startDate: true,
+            isPresale: true,
+          },
+        },
+        transactionPresales: {
+          select: {
+            id: true,
+            users: {
+              select: {
+                id: true,
+                walletAddress: true,
+                fullname: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    if (!result) {
+      throw new NotFoundException(`data with ${id} not found!`);
+    }
+    return result;
+  }
+  // TODO: update
+
+  // Action Extra
+  async updateReviewProject(dto: UpdateProjectDto) {
+    return dto;
+  }
+  // Action Extra
+
+  private async withPagination(query: QueryParamDto) {
+    const paginate = createPaginator({
+      page: query.page,
+      perPage: query.pageSize,
+    });
+    const orderField = query.sortBy || 'createdAt';
+    const orderType = query.sortType || 'desc';
+    const orderBy = { [orderField]: orderType };
+    const result = await paginate<Project, Prisma.ProjectFindManyArgs>(
+      this.prisma.project,
+      {
+        where: {
+          OR: query?.search
+            ? [{ name: { contains: query.search, mode: 'insensitive' } }]
+            : undefined,
+        },
+        orderBy,
+        select: {
+          id: true,
+          name: true,
+          slug: true,
+          logo: true,
+          banner: true,
+          ticker: true,
+          decimals: true,
+          totalSupply: true,
+          detail: true,
+          status: true,
+          userId: true,
+          socials: {
+            select: {
+              url: true,
+              social: {
+                select: {
+                  id: true,
+                  name: true,
+                  icon: true,
+                },
+              },
+            },
+          },
+          category: {
+            select: {
+              id: true,
+              name: true,
+              icon: true,
+            },
+          },
+          chains: {
+            select: {
+              chain: {
+                select: {
+                  id: true,
+                  name: true,
+                  logo: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    );
+    return result;
+  }
+  private async noPagination(query: QueryParamDto) {
+    const orderField = query.sortBy || 'createdAt';
+    const orderType = query.sortType || 'desc';
+    const orderBy = { [orderField]: orderType };
+    return this.prisma.project.findMany({
+      where: {
+        OR: query?.search
+          ? [{ name: { contains: query.search, mode: 'insensitive' } }]
+          : undefined,
+      },
+      orderBy,
+      select: {
+        id: true,
+        name: true,
+        slug: true,
+        logo: true,
+        banner: true,
+        ticker: true,
+        decimals: true,
+        totalSupply: true,
+        detail: true,
+        status: true,
+        userId: true,
+        socials: {
+          select: {
+            url: true,
+            social: {
+              select: {
+                id: true,
+                name: true,
+                icon: true,
+              },
+            },
+          },
+        },
+        category: {
+          select: {
+            id: true,
+            name: true,
+            icon: true,
+          },
+        },
+        chains: {
+          select: {
+            chain: {
+              select: {
+                id: true,
+                name: true,
+                logo: true,
+              },
+            },
+          },
+        },
       },
     });
   }
